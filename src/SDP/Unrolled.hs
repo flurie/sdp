6e1288b029cbfed4d223850a2a24abbdeb26833d
Andrey Mulik <work.a.mulik@gmail.com>
2019-04-18 19:35:06 +0300

    size removed from Unrolled

--- src/SDP/Unrolled.hs
+++ src/SDP/Unrolled.hs
@@ -44,8 +44,7 @@ default ()
 data Unrolled i e = Unrolled
                   !i                  {- lower  bound -}
                   !i                  {- upper  bound -}
-                  {-# UNPACK #-} !Int {-     size     -}
-                  (UNList e)          {-  container   -}
+                  (Unlist e)          {-  container   -}
 
 --------------------------------------------------------------------------------
 
@@ -55,10 +54,10 @@ instance (Eq e, Index i) => Eq (Unrolled i e) where (==) = eq1
 
 instance (Index i) => Eq1 (Unrolled i)
   where
-    liftEq f unr1 unr2 = n1 == n2 && (null unr1 && null unr2 || l1 == l2 && u1 == u2 && liftEq f xs ys)
+    liftEq f unr1 unr2 = null xs && null xs || l1 == l2 && u1 == u2 && liftEq f xs ys
       where
-        (Unrolled l1 u1 n1 xs) = unr1
-        (Unrolled l2 u2 n2 ys) = unr2
+        (Unrolled l1 u1 xs) = unr1
+        (Unrolled l2 u2 ys) = unr2
 
 --------------------------------------------------------------------------------
 
@@ -78,9 +77,9 @@ instance (Index i) => Ord1 (Unrolled i)
 
 instance (Index i, Show i, Show e) => Show (Unrolled i e)
   where
-    showsPrec p arr@(Unrolled l u _ _) = showParen (p > appPrec) shows'
+    showsPrec p unr@(Unrolled l u _) = showParen (p > appPrec) shows'
       where
-        shows' = showString "unrolled " . shows (l, u) . showChar ' ' . shows (assocs arr)
+        shows' = showString "unrolled " . shows (l, u) . showChar ' ' . shows (assocs unr)
 
 instance (Index i, Read i, Read e) => Read (Unrolled i e)
   where
@@ -93,7 +92,7 @@ instance (Index i, Read i, Read e) => Read (Unrolled i e)
 
 instance (Index i) => Functor (Unrolled i)
   where
-    fmap f (Unrolled l u n es) = Unrolled l u n (f <$> es)
+    fmap f (Unrolled l u es) = Unrolled l u (f <$> es)
 
 -- instance (Index i) => Zip (Unrolled i)
 
@@ -108,19 +107,19 @@ instance (Index i) => Applicative (Unrolled i)
 
 instance (Index i) => Foldable (Unrolled i)
   where
-    foldr  f base (Unrolled _ _ _ es) = foldr  f base es
-    foldl  f base (Unrolled _ _ _ es) = foldl  f base es
+    foldr  f base (Unrolled _ _ es) = foldr  f base es
+    foldl  f base (Unrolled _ _ es) = foldl  f base es
     
-    foldr' f base (Unrolled _ _ _ es) = foldr' f base es
-    foldl' f base (Unrolled _ _ _ es) = foldl' f base es
+    foldr' f base (Unrolled _ _ es) = foldr' f base es
+    foldl' f base (Unrolled _ _ es) = foldl' f base es
     
-    foldr1 f (Unrolled _ _ _ es) = foldr1 f es
-    foldl1 f (Unrolled _ _ _ es) = foldl1 f es
+    foldr1 f (Unrolled _ _ es) = foldr1 f es
+    foldl1 f (Unrolled _ _ es) = foldl1 f es
     
-    length (Unrolled _ _ n  _) = n
-    toList (Unrolled _ _ _ es) = toList es
-    elem e (Unrolled _ _ _ es) = e `elem` es
-    null   (Unrolled l u n es) = null es || n < 1 || isEmpty (l, u)
+    length (Unrolled l u  _) = size (l, u)
+    toList (Unrolled l u es) = take n $ toList es where n = size (l, u)
+    elem e (Unrolled _ _ es) = e `elem` es
+    null   (Unrolled l u es) = null es || isEmpty (l, u)
 
 -- instance (Index i) => Scan (Unrolled i)
 
@@ -134,28 +133,26 @@ instance (Index i) => Traversable (Unrolled i)
 
 instance (Index i) => Linear (Unrolled i)
   where
-    fromListN n es = Unrolled l u n $ fromListN n es
+    fromListN n es = Unrolled l u $ fromListN n es
       where
         l = unsafeIndex 0
         u = unsafeIndex $ max 0 n - 1
     
     uncons Z = throw $ EmptyRange "in SDP.Unrolled.(:>)"
-    uncons (Unrolled l u n es) = (x, n < 2 ? Z $ Unrolled l1 u n1 xs)
+    uncons (Unrolled l u es) = (x, es <. 2 ? Z $ Unrolled l1 u xs)
       where
         (x, xs) = uncons es
         l1 = next (l, u) l
-        n1 = n - 1
     
     unsnoc Z = throw $ EmptyRange "in SDP.Unrolled.(:<)"
-    unsnoc (Unrolled l u n es) = (n < 2 ? Z $ Unrolled l u1 n1 xs, x)
+    unsnoc (Unrolled l u es) = (es <. 2 ? Z $ Unrolled l u1 xs, x)
       where
         (xs, x) = unsnoc es
         u1 = prev (l, u) u
-        n1 = n - 1
     
-    concat xss = Unrolled l u n' res
+    concat xss = Unrolled l u res
       where
-        (n', res) = foldr (\ (Unrolled _ _ n xs) (len, ys) -> (len + n, xs ++ ys)) (0, Z) xss
+        (n', res) = foldr (\ (Unrolled _ _ xs) (len, ys) -> (len + length xs, xs ++ ys)) (0, Z) xss
         
         l = unsafeIndex 0
         u = unsafeIndex $ max 0 n' - 1
@@ -164,30 +161,37 @@ instance (Index i) => Linear (Unrolled i)
     
     -- isSubseqOf
     
-    intersperse e (Unrolled _ _ n es) = Unrolled l1 u1 n1 (intersperse e es)
+    intersperse e (Unrolled _ _ es) = Unrolled l1 u1 (intersperse e es)
       where
-        n1 = case n <=> 0 of {LT -> -1; EQ -> 1; GT -> 2 * n - 1}
-        u1 = unsafeIndex (n1 - 1)
+        n1 = case n <=> 0 of {LT -> -2; EQ -> 0; GT -> 2 * n - 2}
+        n  = length es
+        u1 = unsafeIndex n1
         l1 = unsafeIndex 0
 
 instance (Index i) => Split (Unrolled i)
   where
-    take n (Unrolled l u c es) = Unrolled l u' n' (take n' es)
+    take n unr@(Unrolled l u es)
+      | n <= 0 = Z
+      | n >= c = unr
+      |  True  = Unrolled l u' (take n es)
       where
-        u' = index (l, u) n'
-        n' = min c n
+        u' = index (l, u) $ n - 1
+        c  = size  (l, u)
     
-    drop n (Unrolled l u c es) = Unrolled l' u n' (drop n' es)
+    drop n unr@(Unrolled l u es)
+      | n >= c = Z
+      | n <= 0 = unr
+      |  True  = Unrolled l' u (drop n es)
       where
-        l' = index (l, u) n'
-        n' = min c n
+        l' = index (l, u) $ n - 1
+        c  = size  (l, u)
 
 instance (Index i) => Bordered (Unrolled i) i
   where
-    indices (Unrolled l u _  _) = range (l, u)
-    bounds  (Unrolled l u _  _) = (l, u)
-    lower   (Unrolled l _ _  _) = l
-    upper   (Unrolled _ u _  _) = u
+    indices (Unrolled l u _) = range (l, u)
+    bounds  (Unrolled l u _) = (l, u)
+    lower   (Unrolled l _ _) = l
+    upper   (Unrolled _ u _) = u
 
 --------------------------------------------------------------------------------
 
@@ -206,24 +210,23 @@ instance (Index i) => Indexed (Unrolled i) i
         l = minimum $ fsts ascs
         u = maximum $ fsts ascs
     
-    (Unrolled l u _ es) // ascs = Unrolled l' u' n es'
+    (Unrolled l u es) // ascs = Unrolled l' u' es'
       where
         ascs' = (\ (i, e) -> (offset (l, u) i, e)) <$> ascs
         l'    = unsafeIndex $ lower es'
         u'    = unsafeIndex $ upper es'
-        n     = size (l', u')
         es'   = es // ascs'
     
-    (Unrolled l u _ es)   .! i = es !# (offset (l, u) i)
+    (Unrolled l u es)   .! i = es !# (offset (l, u) i)
     
-    (!) (Unrolled l u _ es)  i = es !# (offset (l, u) i)
+    (!) (Unrolled l u es)  i = es !# (offset (l, u) i)
     
-    (Unrolled l u _ arrs) !? i = inRange (l, u) i ? Just e $ Nothing
+    (Unrolled l u arrs) !? i = inRange (l, u) i ? Just e $ Nothing
       where
         e = arrs !# offset (l, u) i
     
-    predicate .$ (Unrolled l u _ es) = index (l, u) <$> (predicate .$ es)
-    predicate *$ (Unrolled l u _ es) = Unrolled l' u' n es'
+    predicate .$ (Unrolled l u es) = index (l, u) <$> (predicate .$ es)
+    predicate *$ (Unrolled l u es) = Unrolled l' u' es'
       where
         es' = index (l, u) <$> (predicate *$ es)
         u'  = index (l, u) (n - 1)
@@ -240,7 +243,7 @@ instance (Index i, Arbitrary e) => Arbitrary (Unrolled i e)
 
 instance (Index i) => Estimate (Unrolled i)
   where
-    (Unrolled _ _ n1 _) <==> (Unrolled _ _ n2 _) = n1 <=> n2
+    (Unrolled _ _ xs) <==> (Unrolled _ _ ys) = xs <==> ys
 
 instance (Index i) => LineS (Unrolled i)
 
@@ -252,55 +255,36 @@ instance (Index i) => Default (Unrolled i e) where def = Z
 {- INTERNAL SECTION -}
 {--------------------}
 
-data UNList e = UNEmpty | UNList {-# UNPACK #-} !Int (Array# e) (UNList e)
+data Unlist e = UNEmpty | Unlist {-# UNPACK #-} !Int (Array# e) (Unlist e)
 
 --------------------------------------------------------------------------------
 
-{- Eq and Eq1 instances. -}
+{- Eq1 and Ord1 instances. -}
 
-instance (Eq e) => Eq (UNList e) where (==) = eq1
+instance Eq1 Unlist where liftEq f xs ys = liftEq f (toList xs) (toList ys)
 
-instance Eq1 UNList where liftEq f xs ys = liftEq f (toList xs) (toList ys)
-
---------------------------------------------------------------------------------
-
-{- Ord and Ord1 instances. -}
-
-instance (Ord e) => Ord (UNList e) where compare = compare1
-
-instance Ord1 UNList where liftCompare f xs ys = liftCompare f (toList xs) (toList ys)
-
---------------------------------------------------------------------------------
-
-{- [internal]: Show instance. -}
-
-instance (Show e) => Show (UNList e)
-  where
-    showsPrec p UNEmpty = showParen (p > appPrec) $ showString "UNEmpty"
-    showsPrec p es = showParen (p > appPrec) shows'
-      where
-        shows' = showString "unlist " . shows (toList es)
+instance Ord1 Unlist where liftCompare f xs ys = liftCompare f (toList xs) (toList ys)
 
 --------------------------------------------------------------------------------
 
 {- Functor, Zip and Applicative instances. -}
 
-instance Functor UNList
+instance Functor Unlist
   where
     fmap _ UNEmpty = UNEmpty
-    fmap f arr@(UNList n@(I# n#) _ arrs) = runST
+    fmap f arr@(Unlist n@(I# n#) _ arrs) = runST
         (
           ST $ \ s1# ->
             case newArray# n# (undEx "fmap") s1# of
               (# s2#, marr# #) ->
                 let go i s3# = if i == n
                     then case unsafeFreezeArray# marr# s3# of
-                      (# s4#, arr# #) -> (# s4#, UNList n arr# (f <$> arrs) #)
+                      (# s4#, arr# #) -> (# s4#, Unlist n arr# (f <$> arrs) #)
                     else fill marr# (i, f $ arr !# i) (go $ i + 1) s3#
                 in go 0 s2#
         )
 
-instance Zip UNList
+instance Zip Unlist
   where
     zipWith  f as bs             = fromList $ zipWith  f (toList as) (toList bs)
     zipWith3 f as bs cs          = fromList $ zipWith3 f (toList as) (toList bs) (toList cs)
@@ -308,67 +292,56 @@ instance Zip UNList
     zipWith5 f as bs cs ds es    = fromList $ zipWith5 f (toList as) (toList bs) (toList cs) (toList ds) (toList es)
     zipWith6 f as bs cs ds es fs = fromList $ zipWith6 f (toList as) (toList bs) (toList cs) (toList ds) (toList es) (toList fs)
 
-instance Applicative UNList
-  where
-    pure = single
-    fs <*> es = (<$> es) `concatMap` fs
-
 --------------------------------------------------------------------------------
 
 {- Foldable, Zip and Traversable instances. -}
 
-instance Foldable UNList
+instance Foldable Unlist
   where
     foldr _ base Z = base
-    foldr f base (UNList c arr# arrs) = go (foldr f base arrs) 0
+    foldr f base (Unlist c arr# arrs) = go (foldr f base arrs) 0
       where
         go b i@(I# i#) = i == c ? b $ f e (go b $ i + 1)
           where
             (# e #) = indexArray# arr# i#
     
     foldr' _ base Z = base
-    foldr' f base (UNList c arr# arrs) = go (foldr' f base arrs) 0
+    foldr' f base (Unlist c arr# arrs) = go (foldr' f base arrs) 0
       where
         go b i@(I# i#) = i == c ? b $ f e (go b $ i + 1)
           where
             (# e #) = indexArray# arr# i#
     
     foldl _ base Z = base
-    foldl f base (UNList c arr# arrs) = foldl f (go base $ c - 1) arrs
+    foldl f base (Unlist c arr# arrs) = foldl f (go base $ c - 1) arrs
       where
         go b i@(I# i#) = -1 == i ? b $ f (go b $ i - 1) e
           where
             (# e #) = indexArray# arr# i#
     
     foldl' _ base Z = base
-    foldl' f base (UNList c arr# arrs) = foldl' f (go base c) arrs
+    foldl' f base (Unlist c arr# arrs) = foldl' f (go base c) arrs
       where
         go b i@(I# i#) = -1 == i ? b $ f (go b $ i - 1) e
           where
             (# e #) = indexArray# arr# i#
     
     length Z = 0
-    length (UNList c _ arrs) = c + (length arrs)
+    length (Unlist c _ arrs) = c + (length arrs)
     
     toList Z = []
-    toList (UNList c arr# arrs) = foldr addIxElem (toList arrs) [0 .. c - 1]
+    toList (Unlist c arr# arrs) = foldr addIxElem (toList arrs) [0 .. c - 1]
       where
         addIxElem (I# i#) = let (# e #) = indexArray# arr# i# in (e :)
     
     null UNEmpty = True
-    null (UNList c _ _) = c < 0
-
--- instance Scan UNList
-
-instance Traversable UNList
-  where
-    traverse f arr = fromList <$> traverse f (toList arr)
+    null (Unlist c _ _) = c < 0
 
 --------------------------------------------------------------------------------
 
 {- Linear, Split and Bordered instances. -}
 
-instance Linear UNList
+instance Linear Unlist
   where
     fromList [] = UNEmpty
     fromList es = runST $ ST
@@ -395,25 +368,25 @@ instance Linear UNList
     last es = es !# (length es - 1)
     
     tail Z  = throw $ EmptyRange "in SDP.Unrolled.(:<)"
-    tail es@(UNList _ _ Z) = fromList . tail $ toList es
-    tail (UNList c arr# arrs) = UNList c' new# arrs
+    tail es@(Unlist _ _ Z) = fromList . tail $ toList es
+    tail (Unlist c arr# arrs) = Unlist c' new# arrs
       where
-        !(UNList c' new# _) = tail (UNList c arr# Z)
+        !(Unlist c' new# _) = tail (Unlist c arr# Z)
     
     init Z = throw $ EmptyRange "in SDP.Unrolled.(:>)"
-    init es@(UNList _ _ Z) = fromList . init $ toList es
-    init (UNList c arr# arrs) = UNList c arr# (init arrs)
+    init es@(Unlist _ _ Z) = fromList . init $ toList es
+    init (Unlist c arr# arrs) = Unlist c arr# (init arrs)
     
     Z ++ ys = ys
     xs ++ Z = xs
-    (UNList c arr# arrs) ++ ys = UNList c arr# (arrs ++ ys)
+    (Unlist c arr# arrs) ++ ys = Unlist c arr# (arrs ++ ys)
     
     replicate n e = copy count chunk
       where
-        copy c ch@(UNList _ chunk# _) = case c <=> 0 of
+        copy c ch@(Unlist _ chunk# _) = case c <=> 0 of
           LT -> Z
           EQ -> fromListN restsize (repeat e)
-          GT -> UNList lim chunk# (copy (c - 1) ch)
+          GT -> Unlist lim chunk# (copy (c - 1) ch)
         
         (count, restsize) = n `divMod` lim
         
@@ -421,29 +394,27 @@ instance Linear UNList
         lim   = _UNLIST_CHUNK_MAX_SIZE_
     
     toHead e Z = single e
-    toHead e (UNList c arr# arrs) = c < lim ? res1 $ (UNList 1 single# arrs)
+    toHead e (Unlist c arr# arrs) = c < lim ? res1 $ (Unlist 1 single# arrs)
       where
-        res1 = fromListN (max 0 c + 1) $ e : toList (UNList c arr# Z)
-        !(UNList 1 single# Z) = single e
+        res1 = fromListN (max 0 c + 1) $ e : toList (Unlist c arr# Z)
+        !(Unlist 1 single# Z) = single e
         
         lim  = _UNLIST_CHUNK_MAX_SIZE_
     
     toLast Z e = single e
-    toLast es@(UNList c _ Z) e = c < lim ? res1 $ (UNList 1 single# Z)
+    toLast es@(Unlist c _ Z) e = c < lim ? res1 $ (Unlist 1 single# Z)
       where
         res1 = fromListN (max 0 c + 1) $ foldr (:) [e] es
-        !(UNList 1 single# Z) = single e
+        !(Unlist 1 single# Z) = single e
         
         lim  = _UNLIST_CHUNK_MAX_SIZE_
-    toLast (UNList c arr# arrs) e = UNList c arr# (toLast arrs e)
-    
-    filter predicate = fromList . filter predicate . toList
+    toLast (Unlist c arr# arrs) e = Unlist c arr# (toLast arrs e)
     
     partition predicate es = (fromList x, fromList y)
       where
         (x, y) = partition predicate $ toList es
 
-instance Split UNList
+instance Split Unlist
   where
     take n es
         |  n <= 0  = Z
@@ -451,9 +422,9 @@ instance Split UNList
         |   True   = take' n es
       where
         take' _ Z = Z
-        take' n' (UNList c arr# arrs) = n' > c ? take' (n' - c) arrs $ UNList c1 arr1# arrs
+        take' n' (Unlist c arr# arrs) = n' > c ? take' (n' - c) arrs $ Unlist c1 arr1# arrs
           where
-            !(UNList c1 arr1# _) = fromList . take n' . toList $ UNList c arr# Z
+            !(Unlist c1 arr1# _) = fromList . take n' . toList $ Unlist c arr# Z
     
     drop n es
         |  n <=  0 = es
@@ -461,9 +432,9 @@ instance Split UNList
         |   True   = drop' n es
       where
         drop' _ Z = Z
-        drop' n' (UNList c arr# arrs) = n' > c ? drop' (n' - c) arrs $ UNList c1 arr1# arrs
+        drop' n' (Unlist c arr# arrs) = n' > c ? drop' (n' - c) arrs $ Unlist c1 arr1# arrs
           where
-            !(UNList c1 arr1# _) = fromList . drop n' . toList $ UNList c arr# Z
+            !(Unlist c1 arr1# _) = fromList . drop n' . toList $ Unlist c arr# Z
     
     split n es
         |  n  <  0  = (Z, es)
@@ -471,10 +442,10 @@ instance Split UNList
         |    True   = split' n es
       where
         split' _ Z = (Z, Z)
-        split' n' (UNList c arr# arrs) = n' > c ? (UNList c arr# ts, ds) $ (fromList tpart, UNList c1 arr1# arrs)
+        split' n' (Unlist c arr# arrs) = n' > c ? (Unlist c arr# ts, ds) $ (fromList tpart, Unlist c1 arr1# arrs)
           where
-            (tpart, dpart) = split n' . toList $ UNList c arr# Z
-            !(UNList _ arr1# _) = fromList dpart
+            (tpart, dpart) = split n' . toList $ Unlist c arr# Z
+            !(Unlist _ arr1# _) = fromList dpart
             (ts, ds) = split' c1 arrs
             c1 = n' - c
     
@@ -483,7 +454,7 @@ instance Split UNList
       where
         n = length xs - length ys
 
-instance Bordered UNList Int
+instance Bordered Unlist Int
   where
     lower  _  = 0
     upper  es = length es - 1
@@ -491,7 +462,7 @@ instance Bordered UNList Int
 
 --------------------------------------------------------------------------------
 
-instance Indexed UNList Int
+instance Indexed Unlist Int
   where
     assoc' bnds@(l, u) defvalue ascs = isEmpty bnds ? UNEmpty $ runST
         (
@@ -513,7 +484,7 @@ instance Indexed UNList Int
         l = minimum $ fsts ascs
         u = maximum $ fsts ascs
     
-    es@(UNList c@(I# c#) _ arrs) // ascs = runST $ thaw >>= (`writes` (arrs // others))
+    es@(Unlist c@(I# c#) _ arrs) // ascs = runST $ thaw >>= (`writes` (arrs // others))
       where
         writes (STArray l' u' n' marr#) rest = ST $ foldr (fill marr#) (done n' rest marr#) ies
           where
@@ -547,29 +518,29 @@ instance Indexed UNList Int
 
 --------------------------------------------------------------------------------
 
-instance Estimate UNList
+instance Estimate Unlist
   where
     UNEmpty         <==>         UNEmpty = EQ
-    (UNList c1 _ _) <==>         UNEmpty = c1 <=>  0
-    UNEmpty         <==> (UNList c2 _ _) = 0  <=> c2
-    (UNList c1 _ _) <==> (UNList c2 _ _) = c1 <=> c2
+    (Unlist c1 _ _) <==>         UNEmpty = c1 <=>  0
+    UNEmpty         <==> (Unlist c2 _ _) = 0  <=> c2
+    (Unlist c1 _ _) <==> (Unlist c2 _ _) = c1 <=> c2
 
-instance (Arbitrary e) => Arbitrary (UNList e)
+instance (Arbitrary e) => Arbitrary (Unlist e)
   where
     arbitrary = fromList <$> arbitrary
 
 --------------------------------------------------------------------------------
 
 {-# INLINE (!#) #-}
-(!#) :: UNList e -> Int -> e
-(UNList c arr# arrs) !# i@(I# i#) = i < c ? e $ arrs !# (i - c)
+(!#) :: Unlist e -> Int -> e
+(Unlist c arr# arrs) !# i@(I# i#) = i < c ? e $ arrs !# (i - c)
   where
     (# e #) = indexArray# arr# i#
 
 {-# INLINE done #-}
-done :: Int -> UNList e -> MutableArray# s e -> STRep s (UNList e)
+done :: Int -> Unlist e -> MutableArray# s e -> STRep s (Unlist e)
 done c rest marr# = \s1# -> case unsafeFreezeArray# marr# s1# of
-  (# s2#, arr# #) -> (# s2#, UNList c arr# rest #)
+  (# s2#, arr# #) -> (# s2#, Unlist c arr# rest #)
 
 _UNLIST_CHUNK_MAX_SIZE_ :: Int
 _UNLIST_CHUNK_MAX_SIZE_ =  1024
